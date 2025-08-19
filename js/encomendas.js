9ea294e1867854692d9b1f416401e7a7234b310f
Sabrinabreu <168127664+Sabrinabreu@users.noreply.github.com>
2025-08-18 21:56:31 -0300

    index responsivo

--- js/encomendas.js
+++ js/encomendas.js
@@ -1,1 +1,10 @@
-document.querySelector(".total").textContent = document.querySelector(".qtd").textContent * document.querySelector(".unitario").textContent
+// document.querySelector(".total").textContent = document.querySelector(".qtd").textContent * document.querySelector(".unitario").textContent
+
+// captura as encomendas da tabela 
+var clientes = document.querySelectorAll(".cliente");
+
+// passa por todas as encomendas e calcula o total 
+for (var count = 0; count < clientes.length; count++) {
+
+    clientes[count].querySelector(".total").textContent = clientes[count].querySelector(".qtd").textContent * clientes[count].querySelector(".unitario").textContent 
+}
