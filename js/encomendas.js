3e1c5f7394494d10bacb408d51f860d559b18191
Sabrinabreu <168127664+Sabrinabreu@users.noreply.github.com>
2025-08-25 18:59:13 -0300

    erro se valor invalido

--- js/encomendas.js
+++ js/encomendas.js
@@ -1,10 +1,46 @@
-// document.querySelector(".total").textContent = document.querySelector(".qtd").textContent * document.querySelector(".unitario").textContent
-
-// captura as encomendas da tabela 
 var clientes = document.querySelectorAll(".cliente");
 
-// passa por todas as encomendas e calcula o total 
+// Passa por todas as encomendas e calcula o valor total
 for (var count = 0; count < clientes.length; count++) {
 
-    clientes[count].querySelector(".total").textContent = clientes[count].querySelector(".qtd").textContent * clientes[count].querySelector(".unitario").textContent 
+    // Quantidade e valor unitario da encomenda
+    var tab_qtd = parseInt(clientes[count].querySelector(".qtd").textContent);
+    var tab_unitario = parseFloat(clientes[count].querySelector(".unitario").textContent);
+
+    // Verifica se a quantidade é válida
+    if (isNaN(tab_qtd) || tab_qtd < 1) {
+        clientes[count].querySelector(".qtd").textContent = "Quantidade inválida";
+        let tds = clientes[count].querySelectorAll("td");
+
+        // vai olhar cada linha e, se n for valido, fica vermelho
+        tds.forEach(function(td) {
+            td.style.background = "red";
+            td.style.color = "white";
+        });
+    } 
+    // Verifica se o valor unitário é válido
+    else if (isNaN(tab_unitario) || tab_unitario <= 0) {
+        clientes[count].querySelector(".unitario").textContent = "R$ Unitário inválido!";
+        let tds = clientes[count].querySelectorAll("td");
+
+        // vai olhar cada linha e, se n for valido, fica vermelho
+        tds.forEach(function(td) {
+            td.style.background = "red";
+            td.style.color = "white";
+        });
+    } 
+    // Se ambos forem válidos, calcula o total
+    else {
+        clientes[count].querySelector(".total").textContent = calculaTotal(tab_qtd, tab_unitario);
+    }
+}
+
+function calculaTotal(qtd, unit) {
+    var total = qtd * unit;
+    return formataValor(total);
+}
+
+function formataValor(valor) {
+    var valor_formatado = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    return valor_formatado;
 }
