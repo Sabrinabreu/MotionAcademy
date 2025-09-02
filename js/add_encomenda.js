6d24e7bdb1fb16b2f4ec8d93f473c9af9b1e54c7
Sabrinabreu <168127664+Sabrinabreu@users.noreply.github.com>
2025-09-01 21:56:38 -0300

    forms add encomenda funciona

--- js/add_encomenda.js
+++ js/add_encomenda.js
@@ -1,15 +1,52 @@
 var botaoAdicionar = document.querySelector("#encomenda-btn");
 
 botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
     console.log("botao clicado");
-    adicionar();
+    
+    
+    // pega o formulario
+    var form = document.querySelector("#form-add-encomenda");
+
+    // captura nova encomenda
+    var novaEncomenda = obtemEncomenda(form);
+
+    // pega a tabela de encomendas
+    var tabela = document.querySelector("#tabela-clientes");
+
+    // insere a nova encomenda na tabela
+    tabela.appendChild(montaTR(novaEncomenda));
 
 });
 
-function adicionar() {
+// pega os dados do formulario
+function obtemEncomenda(formulario){
+    var encomenda = {
+        nome: formulario.nome.value,
+        produto: formulario.produto.value,
+        quantidade: formulario.quantidade.value,
+        unitario: formulario.unitario.value
+    }
+    return encomenda;
+}
 
-    var linha = document.createElement("tr");
-    var coluna = document.createElement("td");
+// cria nova linha
+function montaTR(dadosNovaEncomenda){
+    var linha = document.createElement('tr');
+    linha.appendChild(montaTD(dadosNovaEncomenda.nome));
+    linha.appendChild(montaTD(dadosNovaEncomenda.produto));
+    linha.appendChild(montaTD(dadosNovaEncomenda.quantidade));
+    linha.appendChild(montaTD(formataValor(dadosNovaEncomenda.unitario)));
+    linha.appendChild(montaTD(calculaTotal(dadosNovaEncomenda.quantidade, dadosNovaEncomenda.unitario)));
 
-    linha.appendChild(coluna);
-};
+    return linha;
+}
+
+// cria nova coluna
+function montaTD(dadoColuna){
+    var coluna = document.createElement('td');
+
+    coluna.textContent = dadoColuna
+
+    return coluna;
+}
