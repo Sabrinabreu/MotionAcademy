00ed75f5d91af566addf4115115442b554e86378
Sabrinabreu <168127664+Sabrinabreu@users.noreply.github.com>
2025-11-24 15:56:27 -0300

    teste

---
+++ meu-site/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./Componentes/Header";
+import Banner from "./Componentes/Banner";
+import SobreNos from "./Componentes/SobreNos";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <SobreNos />
+    </>
+  );
+}
+
+export default App;
